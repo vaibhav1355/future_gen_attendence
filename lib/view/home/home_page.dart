ebdbee2a097f606beccfd1671bfc7fdf4928bfe2
vaibhav973 <vaibhavwadhwa06@gmail.com>
2024-12-10 18:38:59 +0530

    added showModel bottom sheet

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -10,11 +10,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  final List<Map<String, dynamic>> data = [
-    {"strings": "Admin - General", "times": "00:00", "journals": ""},
-    {"strings": "Academic - General", "times": "10:15", "journals": ""},
-    {"strings": "Fundraising - General", "times": "11:30", "journals": ""},
-  ];
   DateTime selectedDate = DateTime.now();
   final DateTime currentdate = DateTime.now();
 
@@ -48,20 +43,140 @@ class _HomePageState extends State<HomePage> {
           picked.hour,
           picked.minute,
         );
-
-        // Update the 'times' field for the specific item in the data list
         data[index]['times'] = DateFormat('HH:mm').format(newTime);
       });
     }
   }
 
+  bool _islocked = false;
+
+  final List<String> categories = [
+    'Admin-General',
+    'Academic-General',
+    'Fundraising-General',
+    'Marketing-General',
+    'Operations-General',
+    'Finance-General',
+    'HR-General',
+    'Research-General',
+    'Event Management-General',
+    'Customer Service-General',
+  ];
+
+  List<String> selectedCategories = [
+    'Admin-General',
+    'Academic-General',
+    'Fundraising-General'
+  ];
+
+  void _showCategoryBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Column(
+                children: [
+                  // Fixed Top Buttons
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(
+                              color: Color(0xff6C60FF),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context, selectedCategories);
+                          },
+                          child: Text(
+                            'Add Category',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(thickness: 1, color: Color(0xffE7E7E7)),
+
+                  // Scrollable Category List
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            CheckboxListTile(
+                              title: Text(categories[index]),
+                              value: selectedCategories.contains(categories[index]),
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  if (value != null && value) {
+                                    selectedCategories.add(categories[index]);
+                                  } else {
+                                    // selectedCategories.remove(categories[index]);
+                                  }
+                                });
+                              },
+                              controlAffinity: ListTileControlAffinity.leading,
+                            ),
+                            Divider(
+                              thickness: 1,
+                              color: Color(0xffE7E7E7),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    ).then((updatedCategories) {
+      if (updatedCategories != null) {
+        setState(() {
+          for (var category in updatedCategories) {
+            if (!data.any((item) => item["strings"] == category)) {
+              data.add({
+                "strings": category,
+                "times": "00:00",
+                "journals": "",
+              });
+            }
+          }
+        });
+      }
+    });
+  }
+
+  final List<Map<String, dynamic>> data = [
+    {"strings": "Admin-General", "times": "00:00", "journals": ""},
+    {"strings": "Academic-General", "times": "10:15", "journals": ""},
+    {"strings": "Fundraising-General", "times": "11:30", "journals": ""},
+  ];
 
   @override
   Widget build(BuildContext context) {
     String today = DateFormat('EEE, dd MMM yyyy').format(selectedDate);
-
-
-
     return Scaffold(
       appBar: AppBar(
         leading:  Icon(Icons.menu, size: 26, color: Colors.white),
@@ -118,95 +233,103 @@ class _HomePageState extends State<HomePage> {
               itemCount: data.length + 1,
               itemBuilder: (context, index) {
                 if (index == data.length) {
-                  return Card(
-                    margin: EdgeInsets.zero,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 12.0, top: 12.0, bottom: 12.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Image.asset(
-                          'assets/images/add_img.png',
-                          height: 50,
-                          width: 50,
-                          color: Colors.grey,
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 8, left: 20.0, bottom: 8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: InkWell(
+                            onTap: () {
+                              _showCategoryBottomSheet(context);
+                            },
+                            child: Image.asset(
+                              'assets/images/add_img.png',
+                              height: 50,
+                              width: 50,
+                              color: Colors.grey,
+                              semanticLabel: "Add item",
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      Divider(
+                        thickness: 2,
+                        color: Color(0xfff1ecf0),
+                      ),
+                    ],
                   );
                 }
                 final item = data[index];
                 return Column(
                   children: [
-                    Card(
-                      margin: EdgeInsets.zero,
-                      child: ListTile(
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 16,
+                    ListTile(
+                      contentPadding: EdgeInsets.only(left: 30, top: 8, bottom: 8, right: 10),
+                      leading: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6.0),
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width * 0.3,
+                          child: Text(
+                            item['strings'],
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
                         ),
-                        leading: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.30,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 12.0),
-                            child: Text(
-                              item['strings'],
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                      ),
+                      title: InkWell(
+                        onTap: () => _selectTime(context, index),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 10, height: 50),
+                            Flexible(
+                              child: Text(
+                                item['times'],
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
                             ),
-                          ),
-                        ),
-                        title: GestureDetector(
-                          onTap: () => _selectTime(context ,index),
-                          child: Center(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Flexible(
-                                  child: Text(
-                                    item['times'],
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 10),
-                                Image.asset(
-                                  'assets/images/caret_arrow_up.png',
-                                  height: 20,
-                                  width: 20,
-                                ),
-                              ],
+                            SizedBox(width: 25, height: 50),
+                            Image.asset(
+                              'assets/images/caret_arrow_up.png',
+                              height: 20,
+                              width: 20,
+                              semanticLabel: "Select time",
                             ),
-                          ),
+                            SizedBox(width: 5, height: 5),
+                          ],
                         ),
-                        trailing: MaterialButton(
-                          onPressed: () {
-                            // Implement your logic here
-                          },
-                          color: Color(0xffefce21),
-                          minWidth: 120,
-                          padding: EdgeInsets.all(12),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xffefcd1a),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(32.0),
                           ),
-                          child: Text(
-                            'Journal',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Color(0xff0a0a0a),
-                            ),
+                          padding: EdgeInsets.all(12),
+                          minimumSize: Size(110, 38),
+                        ),
+                        child: Text(
+                          'Journal',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff121212),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 5),
+                    Divider(
+                      thickness: 2,
+                      color: Color(0xfff1ecf0),
+                    ),
                   ],
                 );
               },
@@ -215,41 +338,67 @@ class _HomePageState extends State<HomePage> {
           SizedBox(height: 16),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                MaterialButton(
-                  onPressed: () {
-                    // Implement save logic here
-                  },
-                  color: Colors.black,
-                  minWidth: 160,
-                  height: 45,
-                  padding: EdgeInsets.all(12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.0),
+                if (!_islocked) // Show Save and Lock buttons only if not locked
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      MaterialButton(
+                        onPressed: () {
+                          // Implement save logic here
+                        },
+                        color: Colors.black,
+                        minWidth: 160,
+                        height: 45,
+                        padding: const EdgeInsets.all(12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: const Text(
+                          'Save',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: () {
+                          setState(() {
+                            _islocked = true;
+                          });
+                        },
+                        color: Colors.black,
+                        minWidth: 160,
+                        height: 45,
+                        padding: const EdgeInsets.all(12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: const Text(
+                          'Lock',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  child: Text(
-                    'Save',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
+                if (_islocked) // Show the Locked container only if locked
+                  Container(
+                    width: MediaQuery.sizeOf(context).width * 0.8,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Locked',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    // Implement lock logic here
-                  },
-                  color: Colors.black,
-                  minWidth: 160,
-                  height: 45,
-                  padding:  EdgeInsets.all(12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  child: Text(
-                    'Lock',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                ),
               ],
             ),
           ),
